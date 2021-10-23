9a94d829363f85768f6163e22ff3e01a6d60c4bd
nyount14 <90732548+nyount14@users.noreply.github.com>
2021-10-22 20:22:14 -0500

    lesson 12 exercise 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-project';
+  reactiveForm: FormGroup;
+
+  ngOnInit(){
+    this.reactiveForm = new FormGroup({
+      'username' : new FormControl("Nancy", Validators.minLength(5)),
+      'firstname' : new FormControl(null),
+    })
+  }
+
+
+
+  onSubmit(){
+    console.log(this.reactiveForm)
+  }
+
 }
